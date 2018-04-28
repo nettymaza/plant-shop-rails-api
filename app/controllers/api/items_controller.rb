1015ee1d1d731ec8bee3b08b4a1b816afae7e73b
Annette Maza <nettymaza@gmail.com>
2018-04-28 14:37:59 -0600

    finished setting items api

---
+++ app/controllers/api/items_controller.rb
@@ -0,0 +1,47 @@
+class Api::ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :update, :destroy]
+
+  def index
+    render json: Item.all
+  end
+
+  def create
+    item = Item.new(item_params)
+    if item.save
+      render json: item
+    else
+      render json:{ message: item.errors}, status: 400
+    end
+  end
+
+  def show
+    render json: @item
+  end
+
+  def update
+    if @item.update(item_params)
+      render json: @item
+    else
+      render json:{ message: @item.errors}, status: 400
+    end
+  end
+
+  def destroy
+    if @item.destroy
+      render status: 204
+    else
+      render json: { message: "Unable to remove this plant"}, status: 400
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find_by(id: params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :price, :img_url, :likes)
+  end
+
+end
